98a586806b4396c97d81a7d93c4e1cea41f59d28
elizadergaceva4 <114051091+elizadergaceva4@users.noreply.github.com>
2022-09-28 10:02:49 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,35 @@
-human_turn = input('Input human turn: ')
-computer_turn = input('Input computer turn: ')
+#UZDEVUMS 1: Modifice kodu lai spele atkartojas
 
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('Human wins!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('Human wins!')
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('Human wins!')
-else:
-    print('Computer wins!')
+import random
+turns = ['rock', 'paper', 'scissors']
+human_turns = []
+computer_turns = []
+
+while(True):
+    human_turn = input("Enter your turn, or type exit:")
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        print('Thank you for playing! Bye, bye!')
+        break
+
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
+
+
+    print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+    if human_turn == computer_turn:
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('Human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('Human wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('Human wins!')
+    else:
+        print('Computer wins!')
+
+print(f'you have played {len(human_turns)} times')
+print(human_turns)
+print(computer_turns)
